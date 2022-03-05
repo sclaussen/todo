81f8fbf1830dcbee563d1bcb5757eb9469152ebf
Shane Claussen <sclaussen@yahoo.com>
2022-03-04 16:06:12 -0800

    Initial Commit

---
+++ todo/todoApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoApp.swift
+//  todo
+//
+//  Created by Shane Claussen on 3/4/22.
+//
+
+import SwiftUI
+
+@main
+struct todoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
